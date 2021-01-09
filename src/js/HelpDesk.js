bc525cc86016093ac24975bf03d69bcdef9466d8
Maksim <pyshkov.maksim@gmail.com>
2021-01-09 18:30:40 +0200

    feat: full description enable/disable added

--- src/js/HelpDesk.js
+++ src/js/HelpDesk.js
@@ -26,6 +26,8 @@ export default class HelpDesk {
     this.els.helpDesk = this.parent.querySelector(this.selectors.helpDesk);
     this.els.body = this.els.helpDesk.querySelector(this.selectors.body);
     this.els.table = this.els.body.querySelector(this.selectors.table);
+    this.els.table.addEventListener('click', this.onTableClick.bind(this));
+
     console.log('HelpDesk This:', this);
 
     try {
@@ -42,13 +44,50 @@ export default class HelpDesk {
   updateTable(allTickets) {
     this.els.table.innerHTML = '';
     const ticketRowEls = [];
+
     for (const ticket of allTickets) {
       const ticketRowEl = getTicketRow(ticket);
       ticketRowEls.push(ticketRowEl);
-      this.ticketsID.set(ticketRowEl, ticket.id);
+      this.ticketsID.set(ticketRowEl, { id: ticket.id, isFullDescShow: false });
     }
-    console.log('ticketRowEls', ticketRowEls);
+
     this.els.table.append(...ticketRowEls);
     ticketRowEls.length = 0;
+  }
+
+  async onTableClick(event) {
+    console.log('target', event.target);
+    const { target } = event;
+
+    // Определяем было ли нажато на один из элементов, которые подразумевают ответное действие:
+    // поменять статус, показать/скрыть описание, редактировать, удалить. Если нет - выходим.
+    const actionEl = target.closest('[data-table="action"]');
+    console.log('actionEl', actionEl);
+    if (!actionEl) return;
+
+    // Если нажали на показать/скрыть описание.
+    if (actionEl.dataset.action === 'description') {
+      const ticketRowEl = actionEl.closest('[data-table="ticket-row"]');
+      const ticketProp = this.ticketsID.get(ticketRowEl);
+      const ticketFullDescEl = actionEl.querySelector('[data-ticket="description"]');
+
+      // Если полное описание нажатого тикета скрыто, то делаем запрос на сервер и показываем.
+      if (!ticketProp.isFullDescShow) {
+        try {
+          const ticketById = await createRequest({ params: { method: 'ticketById', id: ticketProp.id } });
+          ticketFullDescEl.innerText = ticketById.description;
+          ticketFullDescEl.removeAttribute('data-visibility');
+          ticketProp.isFullDescShow = true;
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.error(e);
+        }
+        // Если полное описание тикета показано, то удаляем его.
+      } else {
+        ticketFullDescEl.dataset.visibility = 'hidden';
+        ticketFullDescEl.innerText = '';
+        ticketProp.isFullDescShow = false;
+      }
+    }
   }
 }
--- src/js/getTicketRow.js
+++ src/js/getTicketRow.js
@@ -3,29 +3,29 @@ export default function getTicketRow(ticket) {
   const time = ticket.created.toLocaleTimeString('ru', { hour: 'numeric', minute: 'numeric' });
   const trEl = document.createElement('tr');
 
+  trEl.dataset.table = 'ticket-row';
   trEl.classList.add('help-desk__row');
   trEl.insertAdjacentHTML('afterbegin', `
-      <td class="help-desk__cell">
-              <!-- !!!!!!!!!!!!!! btn_action ??????? !!!!!!!!!!!!!!!!!!!!!!!! -->
-              <button class="btn btn_type_round btn_action_status">
-                <span data-visibility="v-hidden">Status</span>
-              </button>
-            </td>
-            <td class="help-desk__cell help-desk__coll-name">
-              <span>${ticket.name}</span>
-              <p class="help-desk__description"></p>
-            </td>
-            <td class="help-desk__cell help-desk__coll-created">${date} ${time}</td>
-            <td class="help-desk__cell">
-              <button class="btn btn_type_round btn_action_edit">
-                <span data-visibility="v-hidden">Edit</span>
-              </button>
-            </td>
-            <td class="help-desk__cell">
-              <button class="btn btn_type_round btn_action_delete">
-                <span data-visibility="v-hidden">Delete</span>
-              </button>
-            </td>
-      `);
+    <td class="help-desk__cell">
+      <button data-table="action" data-action="status" data-ticket-status="${ticket.status}" class="btn btn_type_round btn_action_status">
+        <span data-visibility="v-hidden">Status</span>
+      </button>
+    </td>
+    <td data-table="action" data-action="description" class="help-desk__cell help-desk__coll-name">
+      <span>${ticket.name}</span>
+      <p data-ticket="description" data-visibility="hidden" class="help-desk__description"></p>
+    </td>
+    <td class="help-desk__cell help-desk__coll-created">${date} ${time}</td>
+    <td class="help-desk__cell">
+      <button data-table="action" class="btn btn_type_round btn_action_edit">
+        <span data-visibility="v-hidden">Edit</span>
+      </button>
+    </td>
+    <td class="help-desk__cell">
+      <button data-table="action" class="btn btn_type_round btn_action_delete">
+        <span data-visibility="v-hidden">Delete</span>
+      </button>
+    </td>
+   `);
   return trEl;
 }
